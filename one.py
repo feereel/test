277e105fd4547c1a12c7ceb5a651fd98a2c96b30
Afanasev Aleksandr <afanasev.aleksandrlm@gmail.com>
2022-10-22 17:19:26 +0300

    Just for testring merge error

--- one.py
+++ one.py
@@ -1,9 +1,9 @@
 from second import sum_abmultipl
 
 def print_hello(name:str) -> None:
-    print(name)
+    print(name + some stuff)
 
-print()
+print("What are you doing")
 
 def print_number(a:int,b:int) -> int:
     return sum_abmultipl(a,b)
