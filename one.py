18503a29434be5091e3e92807a7fdc7b83e03887
Afanasev Aleksandr <afanasev.aleksandrlm@gmail.com>
2022-10-22 17:20:39 +0300

    And again for testing merge error

--- one.py
+++ one.py
@@ -1,11 +1,11 @@
 from second import sum_abmultipl
 
 def print_hello(name:str) -> None:
-    print(name)
+    print(name + " HI!!!")
 
 print()
 
 def print_number(a:int,b:int) -> int:
     return sum_abmultipl(a,b)
 
-print();
+print("This seems working");
